c8b0bfb3733a780b6bcc030f9f1a7ee7357eb561
Viktoriya <>
2021-02-25 19:35:40 +0700

    Обработка событий

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,11 +23,14 @@ class App extends React.Component {
       }
     ]
   };
+
+  onClickDone = isDone => console.log(isDone);
+
   render() {
     return (<div className={styles.wrap}>
       <h1 className={styles.title}>Важные дела:</h1>
       <InputItem />
-      <ItemList items={this.state.items} />
+      <ItemList items={this.state.items} onClickDone={this.onClickDone} />
       <Footer count={1} />
     </div>);
   }
