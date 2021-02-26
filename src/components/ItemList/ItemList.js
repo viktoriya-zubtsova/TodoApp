13acb5c6cc2e29f1205c6e4858389ae20176ea43
Viktoriya <>
2021-02-26 11:31:03 +0700

    30 урок Изменение состояния. setState

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -6,7 +6,11 @@ import styles from './ItemList.module.css'
 const ItemList = ({ items, onClickDone }) =>(<List>
     {items.map(item => (
       <div key={item.value} className={styles.item}>
-        <Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} />
+        <Item value={item.value}
+        isDone={item.isDone}
+        id={item.id}
+        onClickDone={onClickDone}
+        />
       </div>))}
   </List>);
 
