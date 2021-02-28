576bff791fa107c4564c4b00b578d2d3cc30c1bc
Viktoriya <>
2021-02-28 11:38:30 +0700

    добавила defaultProps и propTypes для Item

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Item from '../Item/Items';
 import List from '@material-ui/core/List';
 import styles from './ItemList.module.css'
+import PropTypes from 'prop-types';
 
 const ItemList = ({ items, onClickDone, onClickDelete }) =>(<List>
     {items.map(item => (
@@ -14,5 +15,17 @@ const ItemList = ({ items, onClickDone, onClickDelete }) =>(<List>
         />
       </div>))}
   </List>);
+
+Item.defaultProps = {
+  value: 'здесь должен быть текст задания',
+  isDone: false,
+  id: Math.floor(Math.random() * Math.floor(100))
+};
+
+Item.propTypes = {
+  value: PropTypes.string.isRequired,
+  isDone: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired
+};
 
 export default ItemList;
