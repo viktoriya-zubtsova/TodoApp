cbaead8e68f18163d4b2d50095cb43602fac75c5
Viktoriya <>
2021-03-15 20:56:05 +0700

    урок 41 Запрос данных с сервера

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Octokit } from '@octokit/rest';
+import styles from './About.module.css';
 
-const About = () =>
-    (<div>
-      <h1>Обо мне</h1>
-    </div>);
+const octokit = new Octokit();
+class About extends React.Component {
+  state = {
+    isLoading: true,
+    repoList: [],
+    error: false
+  }
+  componentDidMount() {
+    octokit.repos.listForUser({
+      username: 'viktoriya-zubtsova'
+    }).then(({ data }) => {
+      this.setState({
+        repoList: data,
+        isLoading: false,
+        userName: data[0].owner.login,
+				userAvatar: data[0].owner.avatar_url
+      });
+      }).catch(error => {
+        this.setState({
+          error: true,
+          isLoading: false,
+          textError: 'Что-то пошло не так...'
+      })
+    })
+  }
+
+  render() {
+    const { isLoading, repoList, error } = this.state;
+
+    if (this.state.error) {
+			return (<div>
+				<h3>{this.state.textError}</h3>
+			</div>);
+		} else {
+    return (
+      <div>
+        <img className={styles.userAvatar} src={this.state.userAvatar}/>
+        <h3>Мой логин на github.com: {this.state.userName}</h3>
+        <h1 className={styles.title}>{ isLoading ? <CircularProgress /> : 'Мои репозитории:'}</h1>
+        {!isLoading && <ol>
+          {repoList.map(repo => (<li key={repo.id}>
+            <a className={styles.link} href={repo.html_url}>{repo.name}</a>
+          </li>))}
+        </ol>}
+      </div>
+    );
+  }
+  }
+}
 
 export default About;
