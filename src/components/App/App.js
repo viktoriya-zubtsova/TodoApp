f7d0546c691a668fe14243af8fdfe4e18eee6dac
Viktoriya <>
2021-04-21 22:11:31 +0700

    no message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,24 @@
 import React from 'react';
 import MetaTags from 'react-meta-tags';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 
 import Todo from '../Todo/Todo';
-import About from '../About/About';
 
 import styles from './App.module.css';
 
 const App = () =>
-    (<Router>
-      <div className={styles.wrap}>
-        <MetaTags>
-            <title>TodoApp</title>
-            <meta property="og:title" content="MyTodoReactApp"/>
-            <meta property="og:image" content="title.png"/>
-            <meta property="og:image:width" content="450"/>
-            <meta property="og:image:height" content="323"/>
-        </MetaTags>
-        <Card className={styles.sidebar}>
-            <Link to='/' className={styles.link}><Button variant="outlined" color="primary">Обо мне</Button></Link>
-            <Link to='/todo' className={styles.link}><Button variant="outlined" color="primary">Дела</Button></Link>
-        </Card>
-        <Card className={styles.content}>
-          <Route path='/' exact component={About} />
-          <Route path='/todo' component={Todo} />
-        </Card>
-      </div>
-    </Router>);
+    (<div className={styles.wrap}>
+      <MetaTags>
+          <title>TodoApp</title>
+          <meta property="og:title" content="MyTodoReactApp"/>
+          <meta property="og:image" content="title.png"/>
+          <meta property="og:image:width" content="450"/>
+          <meta property="og:image:height" content="323"/>
+      </MetaTags>
+      <Card className={styles.content}>
+        <Todo />
+      </Card>
+    </div>);
 
 export default App;
